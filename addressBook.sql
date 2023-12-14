c06bf362babe7970065db38d2291f95860ed44c2
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-14 20:07:10 +0530

    Added:MySQL commands and output for creation of database

--- addressBook.sql
+++ addressBook.sql
@@ -1,2 +1,17 @@
 <--MYSQL COMMANDS FOR ADDRESSBOOK DATABASE-->
-
+<--UC1 Ability to create AddressBookService database-->
+mysql> create database AddressBookService;
+Query OK, 1 row affected (0.01 sec)
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| addressbookservice |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+7 rows in set (0.00 sec)

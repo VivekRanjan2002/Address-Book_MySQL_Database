ceb7b08574d3a488bbfa3a14f6ac020e83be4c4b
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-14 20:10:38 +0530

    Added: MySQL commands and output for creating of table with columns

--- addressBook.sql
+++ addressBook.sql
@@ -15,3 +15,22 @@ mysql> show databases;
 | world              |
 +--------------------+
 7 rows in set (0.00 sec)
+
+<---UC2 create AddressBook table with certain no. of attributes-->
+mysql> create Table AddressBook(
+    -> FirstName varchar(255) NOT NULL,
+    -> LastName varchar(255) NOT NULL,
+    -> Address varchar(255),
+    -> City varchar(100),
+    -> Zip varchar(100),
+    -> PhoneNo varchar(50),
+    -> Email varchar(100));
+Query OK, 0 rows affected (0.10 sec)
+mysql> show tables;
++------------------------------+
+| Tables_in_addressbookservice |
++------------------------------+
+| addressbook                  |
++------------------------------+
+1 row in set (0.00 sec)
+
